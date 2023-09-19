d8d1e98bd7bc1eacdf09bd7d54681f99f7a41579
ricirossi <matheus.ricieri@hotmail.com>
2023-09-19 15:25:01 -0300

    Start new index.scss archive

--- sass-training-2/gulpfile.js
+++ sass-training-2/gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("index.scss").pipe(sass()).pipe(dest("css"));
+  return src("index.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["index.scss"], buildStyles);
+  watch(["index.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
